7e3d1d58c706b771447190b7ea2e857bbf73c94f
Sasha V <sashav4@gmail.com>
2017-11-13 02:09:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function prependDriver(name){
   return prependeddrivers;
 }
 function removeLastDriver(){
-  const noLastDriver = [drivers.slice(-1)]
+  const noLastDriver = drivers.slice(0, drivers.length -1)]
   return noLastDriver;
 }
