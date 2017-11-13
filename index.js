bf32e2d2248fe0030ab0b2362df7755a038ba7f7
Sasha V <sashav4@gmail.com>
2017-11-13 00:57:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"]
-function drivers(){
-  append.drivers("Ralph")
+function destructivelyAppendDriver(){
+  push.drivers("Ralph")
 }
 
 
 
-//destructivelyAppendDriver(name)
-//appends a driver to the end of the drivers array ‣
-//ReferenceError: destructivelyAppendDriver is not defined
-//    at Context.<anonymous> (test/indexTest.js:23:9)
+
